cb5ef7d26333ce6fee0d1fd03d3f91255eb39aff
Roberta de Siqueira <designer.roberta@gmail.com>
2022-04-11 23:34:33 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/devroberta/workshopmongo/domain/Post.java
+++ src/main/java/com/devroberta/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.devroberta.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.devroberta.workshopmongo.dto.AuthorDTO;
+import com.devroberta.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	private Post() {
 	}
@@ -68,6 +73,16 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
