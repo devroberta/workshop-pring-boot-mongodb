0e69134079d3ceeb153a8a8e054827b0559ba745
Roberta de Siqueira <designer.roberta@gmail.com>
2022-04-11 23:55:51 -0300

    Consulta simples com query methods

--- src/main/java/com/devroberta/workshopmongo/controller/PostController.java
+++ src/main/java/com/devroberta/workshopmongo/controller/PostController.java
@@ -1,14 +1,17 @@
 package com.devroberta.workshopmongo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devroberta.workshopmongo.controller.util.URL;
 import com.devroberta.workshopmongo.domain.Post;
-import com.devroberta.workshopmongo.dto.UserDTO;
 import com.devroberta.workshopmongo.services.PostService;
 
 @RestController
@@ -22,5 +25,12 @@ public class PostController {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
